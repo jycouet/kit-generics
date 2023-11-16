ccd9f1f66081fb4cfaf6da45fcf5c77ec4ad87b6
jycouet <jycouet@gmail.com>
2023-11-16 08:21:33 +0100

    init

---
+++ src/lib/Form.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let init: any = {};
+
+  let list = Object.entries(init).map(([key, value]) => ({ key, value }));
+</script>
+
+{#each list as { key, value }, i}
+  <div>
+    <label>
+      <input bind:value />
+      {key}
+    </label>
+  </div>
+{/each}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Form from "$lib/Form.svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<Form init={{ name: "JYC", age: 37 }} />
